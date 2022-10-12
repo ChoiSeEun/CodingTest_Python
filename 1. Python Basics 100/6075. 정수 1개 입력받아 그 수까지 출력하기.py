27a6aa34f5ab7b3ab69832bcb3ed75e31cb5638c
ChoiSeEun <chltpdms0329@naver.com>
2022-10-13 00:01:43 +0900

    221012

---
+++ 1. Python Basics 100/6075. 정수 1개 입력받아 그 수까지 출력하기.py
@@ -0,0 +1,5 @@
+num = int(input())
+print_num = 0
+while(print_num<=num):
+    print(print_num)
+    print_num += 1 
---
+++ 1. Python Basics 100/6079. 언제까지 더해야 할까?.py
@@ -0,0 +1,7 @@
+num = int(input())
+sum = 0 
+plus = 1
+while(sum<num):
+    sum += plus
+    plus += 1
+print(plus-1)
